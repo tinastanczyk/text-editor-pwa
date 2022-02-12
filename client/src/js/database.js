91725170a43db041aad0ed204dd8713f6457fa4a
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-12 14:31:36 -0500

    created getDb method for the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -15,16 +15,35 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
   console.error('PUT to the database');
+  // Create a conection to the database and version we want to use
   const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges (readwrite)
   const tx = jateDb.transaction('jate', 'readwrite');
+  // Open up the desired object store
   const store = tx.objectStore('jate');
+  // Use the .put() method to put the data in the database
   const request = store.put({ id: id, jate: content });
+  // get confirmation of the request
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET all from the database');
+  // Create a connection to the database and version we want to use.
+  const jateDb = await openDB('jate', 1);
+  // Create a new transaction and specify the database and data privileges (readonly)
+  const tx = jateDb.transaction('jate', 'readonly');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+   // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
