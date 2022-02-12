a68af536ac12606d71592c68f2c41b5d54f0469e
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-12 15:08:09 -0500

    trying to debug putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,16 +13,16 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (id, content) => {
+export const putDb = async (content) => {
   console.error('PUT to the database');
-  // Create a conection to the database and version we want to use
+  // Create a connection to the database and version we want to use
   const jateDb = await openDB('jate', 1);
   // Create a new transaction and specify the database and data privileges (readwrite)
   const tx = jateDb.transaction('jate', 'readwrite');
   // Open up the desired object store
   const store = tx.objectStore('jate');
-  // Use the .put() method to put the data in the database
-  const request = store.put({ id: id, jate: content });
+  // Use the .add() method to put the data in the database
+  const request = store.add({ jate: content });
   // get confirmation of the request
   const result = await request;
   console.log('🚀 - data saved to the database', result);
