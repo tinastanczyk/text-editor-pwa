d1ab87b6677a5475846517c5c5b8178a1e788d0d
Tina Stanczyk <t9.stanczyk@gmail.com>
2022-02-12 14:04:04 -0500

    createde all plugins in webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,11 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('PUT to the database');
+  
+
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,12 +22,39 @@ module.exports = () => {
         template: './index.html',
         title: 'J.A.T.E.'
       }),
-      new GenerateSW(),
-      new WebpackPwaManifest()
+      // We already have a service worker file, so we will use inject manifest rather than generating a new sw file. The source is the sw file already in the client folder and the new file name is 'src-sw.js', where the file will be outputted
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        // inject successfully injects the manifest.json file in the index.html
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'A text editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        // publicPath tells webpack where to path this 
+        publicPath: '/',
+        icons: [
+          {
+            // getting icon image from the source
+            src: path.resolve('src/images/logo.png'),
+            // telling manifest what sizes to create the icons
+            sizes: [96, 128, 192, 256, 384, 512],
+            // output goes to the assets folder in the dist folder, not at the root of the dist folder
+            destination: path.join('assets', 'icons'),
+          }
+        ]
+      })
       
     ],
 
     module: {
+      // CSS loaders
       rules: [
         {
           test: /\.css$/i,
@@ -36,6 +63,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
